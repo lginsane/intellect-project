ef7b409c804ce9f7c0155d091ab6432a34d2b4fc
梁傲 <1289206613@qq.com>
2022-04-29 22:18:02 +0800

    build

---
+++ dist/assets/extension.a40147a4.js
@@ -0,0 +1,1 @@
+import{s as t}from"./index.5b82d3e6.js";function o(e){return t({url:"qt/extension/list",method:"get",params:e})}function r(e){return t({url:"qt/extension/add",method:"post",data:e})}function s(e){return t({url:"qt/extension/update",method:"post",data:e})}function u(e){return t({url:"qt/extension/delete",method:"delete",params:e})}export{r as c,u as d,o as g,s as u};
